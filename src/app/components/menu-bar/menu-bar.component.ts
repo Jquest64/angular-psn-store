6d5db78b124b213138532e068fe46bbd58d8dd6a
Marcio Farias Pinto <mjquest64@outlook.com>
2025-02-17 21:01:00 -0300

    Finalizado o desafio de projeto
    Recriando a Interface da PlayStation Store com Angular

--- src/app/components/menu-bar/menu-bar.component.ts
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MenuBarItemComponent } from "./menu-bar-item/menu-bar-item.component";
+import { MenuBarLogoComponent } from "./menu-bar-logo/menu-bar-logo.component";
 
 @Component({
   selector: 'app-menu-bar',
   standalone: true,
   templateUrl: './menu-bar.component.html',
-  styleUrls: ['./menu-bar.component.css']
+  styleUrls: ['./menu-bar.component.css'],
+  imports: [MenuBarItemComponent, MenuBarLogoComponent]
 })
 export class MenuBarComponent { }
